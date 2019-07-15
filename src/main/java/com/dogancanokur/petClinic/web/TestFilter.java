7dd1415aaf585d0e5a22a15c6cc5a9a633d49dae
dogancanokur <ahmet.okur.94@gmail.com>
2019-07-15 04:16:38 +0300

    created custom filter

---
+++ src/main/java/com/dogancanokur/petClinic/web/TestFilter.java
@@ -0,0 +1,22 @@
+package com.dogancanokur.petClinic.web;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(filterName = "/TestServlet")
+public class TestFilter implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        response.getWriter().write("before...");
+        chain.doFilter(request, response);
+        response.getWriter().write("...after");
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
